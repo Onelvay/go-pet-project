fa87da88f435ecd32eba4b42c58b04f34ae2ee6f
Onelvay <abajalen@gmail.com>
2023-03-31 22:22:38 +0600

    comm deleted

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
 import (
-
-	// "net/http"
-	// "os"
-
-	// "github.com/gorilla/mux"
-
 	"fmt"
 	"net/http"
 	"os"
@@ -24,25 +18,5 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	// res, ans := db.GetBookById("d2222")
-	// fmt.Println(res, ans)
-	// for _, i := range db.GetBooks(false) {
-	// 	fmt.Println(i.Price)
-	// }
-	// res := db.GetBooks()
-	// fmt.Println(res)
-	// db.DeleteBookById("11")
-	// fmt.Println(db.GetBooksByName("asd"))
-	// db.CreateBook("abay", 123123, "asdass")
-	// fmt.Println(db.GetBookById("2b5bbd2af3124deca650296428cb36a1"))
-	// var book Book
-	// var books []Book
-	// Db.Raw("SELECT * FROM books LIMIT 1").Scan(&book) //vse knigi limit ybrat'
-	// Db.Create(&Book{Id: "D2212", Price: 100, Created_at: time.Now(), Name: "alenov", Description: "asdas adm qowe kakfk zkkx kckdk kkkkk"})
-	// Db.Find(&books)
-	// fmt.Println(book) // Read
-	// Db.Where("id = ?", "D2222").First(&book)
-	// fmt.Println(books)
-	// Db.Where("id=?", "D2212").Delete(&book)
 
 }
